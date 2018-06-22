6ce1c8ee131d1158bbb4ed9be1f1fe778a41cab6
Chu Song <chuchusong23@gmail.com>
2018-06-21 18:11:39 -0700

    Create database/table.

---
+++ server/migrations/20180621171942_todo.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("todo", (table) => {
+      table.increments();
+      table.text("Title").notNullable();
+      table.integer("Priority").notNullable();
+      table.text("Description");
+      table.boolean("Done").defaultTo("False").notNullable();
+      table.dateTime("Date").notNullable();
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable("todo");
+};
