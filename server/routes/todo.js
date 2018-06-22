b7054b5192c0124701bdea58b3043cc19f0a8a6a
Chu Song <chuchusong23@gmail.com>
2018-06-22 15:53:38 -0700

    Create a new record with POST /todo.

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,15 +1,42 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 const knex = require('../db/knex');
 
 /* This router is mounted at http://localhost:3000/todo  */
-router.get('/', function(req, res, next) {
-    console.log("GET HERE!!!")
+router.get('/', (req, res) => {
     knex("todo").select().then(todos => {
-        console.log(todos)
         res.render('all', { todos: todos });
     })
+});
+
+router.get('/new', (req, res) => {
+   res.render("new");
+});
+
+function validTodo(todo){
+    return typeof todo.Title === "string" && todo.Title.trim() !== "" &&
+            typeof todo.Priority !== "undefined" && !isNaN(Number(todo.Priority));
+};
+
+router.post('/', (req,res) => {
+    if(validTodo(req.body)){
+        const todo = {
+            Title: req.body.Title,
+            Description: req.body.Description,
+            Priority: req.body.Priority,
+            Date: new Date()
+        };
+        knex("todo").insert(todo, "id").then(id => {
+            res.redirect(`todo/${id}`);
+        } )
+    } 
+    else {
+        res.status(500);
+        res.render("error", {
+            message: "Invalid Todo"
+        })
+    }
 });
 
 module.exports = router;
