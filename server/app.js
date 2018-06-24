7a6416d0d48f6946f9018bc7ab5234c00a2c8d16
Chu Song <chuchusong23@gmail.com>
2018-06-24 14:37:24 -0700

    Enabled edit record feature with GET /todo/:id/edit and update record feature with PUT /todo/:id.:

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var methodOverride = require('method-override');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,15 +15,26 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+//Select Helper Function
+const hbs = require('hbs');
+
+hbs.registerHelper('select', function(selected, options) {
+  return options.fn(this).replace(
+      new RegExp(' value=\"' + selected + '\"'),
+      '$& selected="selected"');
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
 app.use('/todo', todoRouter);
 app.use('/users', usersRouter);
+
 
 
 // catch 404 and forward to error handler
--- server/routes/todo.js
+++ server/routes/todo.js
@@ -21,29 +21,53 @@ router.get('/:id', (req, res) => {
     })
 });
 
+router.get('/:id/edit', (req, res) => {
+    const id = req.params.id;
+     knex("todo").select().where("id", id).first().then(todo => {
+        res.render('edit', todo);
+    })
+});
+
 function validTodo(todo){
     return typeof todo.Title === "string" && todo.Title.trim() !== "" &&
             typeof todo.Priority !== "undefined" && !isNaN(Number(todo.Priority));
 };
 
-router.post('/', (req,res) => {
-    if(validTodo(req.body)){
+function validateTodoInsertUpdateRedirect(req, res, callback) {
+    if(validTodo(req.body)) {
         const todo = {
             Title: req.body.Title,
             Description: req.body.Description,
             Priority: req.body.Priority,
-            Date: new Date()
         };
-        knex("todo").insert(todo, "id").then(id => {
-            res.redirect(`todo/${id}`);
-        } )
-    } 
-    else {
+
+        callback(todo);
+    } else {
+        //Respond with an error
         res.status(500);
         res.render("error", {
             message: "Invalid Todo"
-        })
+        });
     }
+}
+router.post('/', (req, res) => {
+    validateTodoInsertUpdateRedirect(req, res, (todo) => {
+        todo.Date = new Date();
+        const id = req.params.id;
+        knex("todo").insert(todo, "id").then(id => {
+            res.redirect(`/todo/${id}`);
+        })
+    })
+});
+
+router.put('/:id', (req, res) => {
+   validateTodoInsertUpdateRedirect(req, res, (todo) => {
+       todo.Date = new Date();
+       const id = req.params.id;   
+       knex("todo").where("id", id).update(todo, "id").then(id => {
+           res.redirect(`/todo/${id}`);
+       })
+   })
 });
 
 module.exports = router;
